54ce9ecafa4beb1957acfe504e828bc465ad1156
Scott Robertson <scott.t.robertson@gmail.com>
2026-03-13 02:25:23 -0400

    fixed a bug where deselect breaks layout

--- src/components/WeatherBar.vue
+++ src/components/WeatherBar.vue
@@ -29,16 +29,6 @@
           <span class="weather-low">{{ tomorrow.low }}°</span>
         </span>
       </div>
-      <div
-        v-if="selectedDay == null"
-        class="weather-unset"
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
-          <line x1="18" y1="6" x2="6" y2="18"/>
-          <line x1="6" y1="6" x2="18" y2="18"/>
-        </svg>
-        <span>No weather filter</span>
-      </div>
     </template>
     <button class="weather-refresh" @click="$emit('refresh')" title="Refresh location">
       <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
